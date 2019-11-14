118b115459ae4fb6527b9ce88969bde621d94813
Aleksandr Sazhin <i4j5@ya.ru>
2019-11-14 12:12:01 +0300

    asana ok

--- src/components/create-lead-project/index.js
+++ src/components/create-lead-project/index.js
@@ -3,7 +3,7 @@ import * as btnTmpl from './btn.pug'
 import * as modalTmpl from './modal.pug'
 import Widget from '../../widget'
 
-export default function createLeadProject(id) {
+export default function createLeadProject(lead_id) {
 
     let widget =  new Widget()
 
@@ -11,37 +11,52 @@ export default function createLeadProject(id) {
 
     $('.card-holder .button-input-more .button-input__context-menu').prepend(btnTmpl())
     
-    // let  ajax = true
-    //$(document).off('click', '#i4j5-create-lead-project')
+    let openModal = true
+    $(document).off('click', '#i4j5-create-lead-project')
     $(document).on('click', '#i4j5-create-lead-project', function(event) {
-        new widget.Modal({
+        openModal = false
+        //$(document).off('submit', '#i4j5-create-lead-project__form')
+
+        let modal = new widget.Modal({
             class_name: 'modal-window',
             init: function ($modal_body) {
                 $modal_body
                     .trigger('modal:loaded')
-                    .html(modalTmpl())
+                    .html(modalTmpl({lead_id}))
                     .trigger('modal:centrify')
                     .append('');
             },
             destroy: function () {
+                openModal = true
+                $(document).off('submit', '#i4j5-create-lead-project__form')
             }
         })
-        // event.stopPropagation()
-        // event.preventDefault()
-        // if (ajax) {
-        //     $('body').addClass('page-loading');
-        //     ajax = false
-        //     $.post(
-        //         `//${widget.settings.server}/api/webhook/amo/create-lead-folders`, 
-        //         {
-        //             id: id
-        //         }
-        //     ).done(function() {
-        //         $('body').removeClass('page-loading')
-        //         //TODO Вызвать окно !!!
-        //         ajax = true
-        //     })
-        // }
+
+        let ajax = true
+        $(document).on('submit','#i4j5-create-lead-project__form', function (event) {
+            event.preventDefault()
+            event.stopPropagation()
+
+            if (ajax) {
+                ajax = false
+                modal.destroy()
+                $('body').addClass('page-loading')
+
+                $.ajax({
+                    url: `//${widget.settings.server}/api/webhook/amo/create-lead-project`,
+                    type: 'post',
+                    contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+                    data: $(this).serialize()
+                }).done(function() {
+                    $('body').removeClass('page-loading')
+                    //TODO Вызвать окно !!!
+                    ajax = true
+                })
+                .always(function() {
+                    console.log($('#i4j5-create-lead-project__form').serialize())
+                })
+            }
+        });
     })
     
 }
